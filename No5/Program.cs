4ce3f9bf86ec6f72138fd6e25f2f02f717a58097
Katherine_20 <mob1911494@gmail.com>
2022-05-27 23:28:22 +0300

    programm #5

---
+++ No5/Program.cs
@@ -0,0 +1,41 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве
+
+int [] ArrayCreation(int length)
+{
+    int [] arr = new int [length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rnd.Next(-10,10);
+    }
+    return arr;
+}
+
+int [] Multiple (int[] original_array) 
+{ 
+    int line = 0; 
+    bool even = false; 
+    if (original_array.Length % 2 == 0) 
+    { 
+        line = original_array.Length / 2; 
+        even = true; 
+    } 
+    else  
+    line = original_array.Length / 2 + 1; 
+    int[] newArray = new int[line]; 
+    for (int i = 0; i < original_array.Length/2; i++) 
+    { 
+        newArray[i] = original_array[i] * original_array[original_array.Length -1 - i]; 
+    } 
+    if (even == false) 
+    { 
+        newArray[newArray.Length - 1] = original_array[original_array.Length/2]; 
+    } 
+    return newArray; 
+}
+Console.WriteLine("Введите размерность массива: ");
+int length = int.Parse(Console.ReadLine());
+int [] original_array = ArrayCreation(length);
+Console.WriteLine(string.Join(" ", original_array));
+int [] Array = Multiple(original_array);
+Console.WriteLine(string.Join(" ", Array));
