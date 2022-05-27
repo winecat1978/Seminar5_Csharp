ac241c0744c1201a81b70877ed4dd6d5edbb2460
Katherine_20 <mob1911494@gmail.com>
2022-05-27 15:27:31 +0300

    programm #33

---
+++ No33/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+
+int [] ArrayCreation(int length)
+{
+    int [] arr = new int [length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rnd.Next(-9,10);
+    }
+    return arr;
+}
+
+string Checkout (int [] array, int secret_num)
+{
+    string CheckResult = " ";
+    int line = array.Length;
+    for (int i = 0; i < line; i++)
+    {
+        if (array[i] == secret_num)
+        {
+            CheckResult = "да";
+        }
+        if (array[i] != secret_num)
+        {
+            CheckResult = "нет";
+        }
+    }
+    return CheckResult;
+}
+
+Console.WriteLine("Введите размерность массива: ");
+int length = int.Parse(Console.ReadLine());
+int [] array = ArrayCreation(length);
+Console.WriteLine(string.Join(" ", array));
+Console.WriteLine("Какое число вы хотите здесь найти?");
+int secret_num = int.Parse(Console.ReadLine());
+string answer = Checkout(array, secret_num);
+Console.WriteLine(answer);
